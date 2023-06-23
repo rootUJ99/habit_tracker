cf2f7d4b66d1ee63aae9e7a1560939f96961b636
ujwal arak <ujwal99arak@gmail.com>
2023-06-23 19:20:18 +0530

    made form scrollable

--- lib/screens/add_edit.dart
+++ lib/screens/add_edit.dart
@@ -115,120 +115,124 @@ class _AddTodoState extends State<AddTodo> {
         FirebaseFirestore.instance.collection('habits');
 
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+      // resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: const Text('add habit'),
       ),
-      body: Form(
-        key: _addEditForm,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                controller: _formControllers['name'],
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'habit name',
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                maxLines: 3,
-                controller: _formControllers['description'],
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'description',
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: ElevatedButton(
-                        onPressed: () async {
-                          TimeOfDay? selectedTimeRTL = await showTimePicker(
-                            context: context,
-                            initialTime: convertToTimeofDay(
-                                '${_formControllers['repeatTime']}'),
-                            initialEntryMode: TimePickerEntryMode.inputOnly,
-                            builder: (BuildContext context, Widget? child) {
-                              return Directionality(
-                                textDirection: TextDirection.ltr,
-                                child: child!,
-                              );
-                            },
-                          );
-                          print(selectedTimeRTL);
-                          if (selectedTimeRTL != null) {
-                            setState(() {
-                              _formControllers['repeatTime'] =
-                                  convertToMicroseconds(selectedTimeRTL);
-                              _formControllers['repeatTimeWithHourMin'] =
-                                  selectedTimeRTL;
-                            });
-                          }
-                        },
-                        child: Text(
-                            'repeat weekly at ${convertToTimeofDay(_formControllers["repeatTime"].toString())}')),
+      body: ListView(
+        children: [
+          Form(
+            key: _addEditForm,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    controller: _formControllers['name'],
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'habit name',
+                    ),
                   ),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Column(
-                children: [
-                  const Text('select duration'),
                   const SizedBox(
-                    height: 10,
+                    height: 20,
                   ),
-                  Wrap(
-                    spacing: 5.0,
-                    children: _duration
-                        .map((item) => ChoiceChip(
-                              label: Text(item['value'].toString()),
-                              selected: item['key'] ==
-                                  _formControllers['duration']['key'],
-                              onSelected: (value) {
+                  TextFormField(
+                    maxLines: 3,
+                    controller: _formControllers['description'],
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'description',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: ElevatedButton(
+                            onPressed: () async {
+                              TimeOfDay? selectedTimeRTL = await showTimePicker(
+                                context: context,
+                                initialTime: convertToTimeofDay(
+                                    '${_formControllers['repeatTime']}'),
+                                initialEntryMode: TimePickerEntryMode.inputOnly,
+                                builder: (BuildContext context, Widget? child) {
+                                  return Directionality(
+                                    textDirection: TextDirection.ltr,
+                                    child: child!,
+                                  );
+                                },
+                              );
+                              print(selectedTimeRTL);
+                              if (selectedTimeRTL != null) {
                                 setState(() {
-                                  _formControllers['duration'] = item;
+                                  _formControllers['repeatTime'] =
+                                      convertToMicroseconds(selectedTimeRTL);
+                                  _formControllers['repeatTimeWithHourMin'] =
+                                      selectedTimeRTL;
                                 });
-                              },
-                            ))
-                        .toList(),
-                  )
+                              }
+                            },
+                            child: Text(
+                                'repeat weekly at ${convertToTimeofDay(_formControllers["repeatTime"].toString())}')),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Column(
+                    children: [
+                      const Text('select duration'),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Wrap(
+                        spacing: 5.0,
+                        children: _duration
+                            .map((item) => ChoiceChip(
+                                  label: Text(item['value'].toString()),
+                                  selected: item['key'] ==
+                                      _formControllers['duration']['key'],
+                                  onSelected: (value) {
+                                    setState(() {
+                                      _formControllers['duration'] = item;
+                                    });
+                                  },
+                                ))
+                            .toList(),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        print("""
+                          ${_formControllers['name']!.text}
+                          ${_formControllers['description']!.text}
+                          ${_formControllers['repeatTime']!.toString()}
+                          ${_formControllers['repeatTimeWithHourMin']}
+                          ${_formControllers['duration']['key']}
+                          ${_formControllers['id']}
+                        """);
+                        if (widget.item != null) {
+                          editHabit(habits);
+                        } else {
+                          addHabit(habits);
+                        }
+                        Navigator.pop(context);
+                      },
+                      child: const Text('save habit'))
                 ],
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    print("""
-                      ${_formControllers['name']!.text}
-                      ${_formControllers['description']!.text}
-                      ${_formControllers['repeatTime']!.toString()}
-                      ${_formControllers['repeatTimeWithHourMin']}
-                      ${_formControllers['duration']['key']}
-                      ${_formControllers['id']}
-                    """);
-                    if (widget.item != null) {
-                      editHabit(habits);
-                    } else {
-                      addHabit(habits);
-                    }
-                    Navigator.pop(context);
-                  },
-                  child: const Text('save habit'))
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
